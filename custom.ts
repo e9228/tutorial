e2befdd414bf409c77ae7e920b41e49b49a4a96a
e9228 <43981246+e9228@users.noreply.github.com>
2020-09-30 10:56:45 +0900

    Update pxt.json, main.blocks, main.ts, tutorial.md, custom.ts

---
+++ custom.ts
@@ -0,0 +1,15 @@
+//% weight=100 color=#03AA74 icon="\uf009"
+namespace ブロック　{
+    //% block="まえすすめ"
+    //% weight=89
+    export function まえすすめ (){
+        basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+    }
+
+}
