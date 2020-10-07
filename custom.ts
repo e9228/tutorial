fb7d17f6091eb359460320a23aef9513c6483f2f
e9228 <43981246+e9228@users.noreply.github.com>
2020-10-07 22:27:40 +0900

    Update pxt.json, main.blocks, main.ts, custom.ts

--- custom.ts
+++ custom.ts
@@ -1,53 +1,29 @@
-//% weight=100 color=#03AA74 icon="\uf009"
-namespace ブロック　{
-    //% block="うしろすすめ"
-    //% weight=86
-    export function うしろすすめ(){
-        basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    `)
-    }
+//% weight=100 color=#03AA74 icon="\uf1b9"
+namespace どうさ　{
+    let wait = 100
+    let FL = 80
+    let BR = 80
+    let BL = 80
     //% block="まえすすめ"
     //% weight=89
-    export function まえすすめ (){
-        basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    `)
+    export function まえすすめ() {
+         kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo2, 85)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo4, 85)
+    basic.pause(wait)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo1, 58)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo3, 78)
+    basic.pause(wait)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo4, 90)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo2, 90)
+    basic.pause(wait)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo2, 95)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo4, 95)
+    basic.pause(wait)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo1, 93)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo3, 113)
+    basic.pause(wait)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo4, 90)
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo2, 90)
+    basic.pause(wait)
     }
-    //% block="とまれ"
-    //% weight=83
-    export function とまれ(){
-        basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    `)
-    }
-    //% block="みぎまわれ"
-    //% weight=83
-    export function みぎまわれ(){
-     basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    `)   
-    }
-    //% block="ひだりまわれ"
-    //% weight=83
-    export function ひだりまわれ(){
-
-    }
-    
 }
--- main.ts
+++ main.ts
@@ -1,11 +1,1 @@
-input.onButtonPressed(Button.A, function () {
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    ブロック.まえすすめ()
-    ブロック.うしろすすめ()
-})
+
