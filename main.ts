8230913048e7488f69f757be7a63b1865c19b73f
e9228 <43981246+e9228@users.noreply.github.com>
2020-11-12 08:18:12 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,1 @@
-input.onButtonPressed(Button.A, function () {
-    どうさ.まえすすめ()
-})
-basic.forever(function () {
-    basic.showLeds(`
-        . . # . .
-        . . # . .
-        . . # . .
-        . . # . .
-        . . # . .
-        `)
-})
+
